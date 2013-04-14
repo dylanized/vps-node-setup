c34a173c270edb93a25d15b57860e35a84e94433
Dylan Hassinger <d@dylanized.com>
2013-04-13 20:33:21 -0500

    Removed profile code

--- vps.sh
+++ vps.sh
@@ -75,26 +75,10 @@ npm -g install sails
 read -p "INSTALL JAM (frontend package manager)"
 npm install -g jamjs
 
-read -p "MODIFY BASH PROFILE (don't worry, it will be backed up)"
-cp $BASH_PROFILE $BASH_PROFILE.orig
-
-# ADD THESE LINES TO $BASH_PROFILE
-#
-# # NVM BASH COMPLETION
-# source ~/nvm/nvm.sh
-# [[ -r $NVM_DIR/bash_completion ]] && . $NVM_DIR/bash_completion
-#
-# # LOAD DOTFILES
-# source ~/.dotfiles/dotfile
-# source ~/.dotfiles/gitfile
-# source ~/.dotfiles/nodefile
-#
-# # PROFILE OVERRIDES, IF NOT ~/.bash_profile
-# alias profile="vim $BASH_PROFILE"
-# alias reload="source $BASH_PROFILE"
-#
-
+echo ""
 echo "All Done!"
+echo ""
+echo "Now run modify_profile.sh and setup_sites.sh"
 
 # Node instructions from:
 # http://qugstart.com/blog/node-js/how-to-install-node-js-on-ubuntu-and-create-an-app-in-5-minutes/
